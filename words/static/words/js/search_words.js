e73d1640685725ffaad1ccdc9b9d9fb561ad429f
Theo Cole <theo.j.cole@gmail.com>
2018-11-29 19:16:00 +0000

    removed bulky test code

--- words/static/words/js/search_words.js
+++ words/static/words/js/search_words.js
@@ -1,20 +1,5 @@
 $(document).ready(function() {
-    var content = [
-        { category: 'South America', title: 'Brazil' },
-        { category: 'South America', title: 'Peru' },
-        { category: 'North America', title: 'Canada' },
-        { category: 'Asia', title: 'South Korea' },
-        { category: 'Asia', title: 'Japan' },
-        { category: 'Asia', title: 'China' },
-        { category: 'Europe', title: 'Denmark' },
-        { category: 'Europe', title: 'England' },
-        { category: 'Europe', title: 'France' },
-        { category: 'Europe', title: 'Germany' },
-        { category: 'Africa', title: 'Ethiopia' },
-        { category: 'Africa', title: 'Nigeria' },
-        { category: 'Africa', title: 'Zimbabwe' },
-    ];
-
+    var content;
     $.getJSON('/words', function(result) {
         $('.ui.search')
             .search({
