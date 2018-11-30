7ae65084a668f9238f676b5ff8171d07f1d2cc95
Theo Cole <theo.j.cole@gmail.com>
2018-11-30 10:39:59 +0000

    move whitenoise into middleware

--- glossary/settings.py
+++ glossary/settings.py
@@ -52,12 +52,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-MIDDLEWARE_CLASSES = [
-    # simplified static service
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'glossary.urls'
