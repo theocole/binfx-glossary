5b98c1ff1fb0723ae3a7212541be26262807f108
Theo Cole <theo.j.cole@gmail.com>
2018-11-30 10:34:13 +0000

    fix to static

--- glossary/settings.py
+++ glossary/settings.py
@@ -127,3 +127,7 @@ USE_TZ = True
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
