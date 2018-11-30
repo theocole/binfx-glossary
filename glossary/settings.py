dbc0e12a14be7671b08ad23d9bd916f846046d68
Theo Cole <theo.j.cole@gmail.com>
2018-11-30 10:27:19 +0000

    move static files

--- glossary/settings.py
+++ glossary/settings.py
@@ -124,5 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
